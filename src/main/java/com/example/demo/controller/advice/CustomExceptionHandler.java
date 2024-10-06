6521f1aaa33f9b92bf5abb97755da8afbff782ce
Aaron Ryu <fbcndah@gmail.com>
2024-10-07 01:26:36 +0900

    실습 5-67: @ControllerAdvice + @ResponseBody = @RestControllerAdvice

--- src/main/java/com/example/demo/controller/advice/CustomExceptionHandler.java
+++ src/main/java/com/example/demo/controller/advice/CustomExceptionHandler.java
@@ -10,17 +10,17 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
-@ControllerAdvice/* Controller 앞단에서 발생하는 에러를 캐치하기 위함 */
+@RestControllerAdvice/* Controller 앞단에서 발생하는 에러를 캐치하기 위함 */
 public class CustomExceptionHandler {
 
     /* @ExceptionHandler 로 어떤 에러인지?를 명시하여 그에 해당하는 에러가 발생 시 바로 아래 정의된 메서드에서 처리를 한다. */
     @ExceptionHandler // @ExceptionHandler(value = {UserNotFoundException.class, NullPointerException.class})
-    @ResponseBody
     public BaseResponse<Void> handle(CustomException e) {
         ExceptionType type = e.getType();
         log.atLevel(type.getLevel()).setCause(e).log(e.getMessage());
@@ -28,7 +28,6 @@ public class CustomExceptionHandler {
     }
 
     @ExceptionHandler // @ExceptionHandler(MethodArgumentNotValidException.class)
-    @ResponseBody
     public BaseResponse<List<FieldErrorDto>> handle(MethodArgumentNotValidException e) {
         List<FieldErrorDto> errors = new ArrayList<>();
         StringBuilder messageBuilder = new StringBuilder();
@@ -42,7 +41,6 @@ public class CustomExceptionHandler {
     }
 
     @ExceptionHandler
-    @ResponseBody
     public BaseResponse<Void> handle(Exception e) {
         log.error(e.getMessage(), e);
         return BaseResponse.failure(ExceptionType.UNCLASSIFIED_ERROR);
