003fa20c8d5495ac3f87bbcf00e4a77e06c18073
Aaron Ryu <fbcndah@gmail.com>
2024-11-15 14:45:07 +0900

    실습 6-4: API 로직 교체 - 기존 Map 자료구조 기반 Repository 을 JDBC API 로 대체

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -2,8 +2,11 @@ package com.example.demo.service;
 
 import com.example.demo.controller.dto.UserResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.sql.SQLException;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -11,10 +14,15 @@ import java.util.List;
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+    private final UserJdbcApiDao userJdbcRepository;
 
     public UserResponseDto findById(Integer id) {
-        User user = userRepository.findById(id);
-        return UserResponseDto.from(user);
+        try {
+            User user = userJdbcRepository.findById(id);
+            return UserResponseDto.from(user);
+        } catch (SQLException e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "자원 반납 시 문제가 있습니다.");
+        }
     }
 
     public List<UserResponseDto> findAll() {
