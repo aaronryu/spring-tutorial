1cef8c6f530f9b3bef283ff877f787b0ebbefa7d
Aaron Ryu <fbcndah@gmail.com>
2024-11-16 00:31:52 +0900

    실습 6-10: JDBCTemplate 활용 #2 전체 유저 조회(SELECT) - findAll

--- src/main/java/com/example/demo/service/UserJdbcTemplateDao.java
+++ src/main/java/com/example/demo/service/UserJdbcTemplateDao.java
@@ -5,6 +5,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.time.ZoneId;
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
@@ -29,5 +30,23 @@ public class UserJdbcTemplateDao {
                 ),
                 getUserParams
         );
+    }
+
+    public List<User> findAll() {
+        String getUserQuery = "SELECT * FROM \"user\"";
+        return this.jdbcTemplate.queryForStream(
+                getUserQuery,
+                (resultSet, rowNum) -> new User(
+                        resultSet.getInt("id"),
+                        resultSet.getString("name"),
+                        resultSet.getInt("age"),
+                        resultSet.getString("job"),
+                        resultSet.getString("specialty"),
+                        resultSet.getTimestamp("created_at")
+                                .toInstant()
+                                .atZone(ZoneId.systemDefault())
+                                .toLocalDateTime()
+                )
+        ).toList();
     }
 }
