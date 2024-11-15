f2c4710b7564bf896e77bde77c6e50022cfbd3e5
Aaron Ryu <fbcndah@gmail.com>
2024-11-15 21:26:26 +0900

    실습 6-5: (B) JDBC API - ResultSet < Statement < Connection (DataSource - HikariCP) #1 객체 직접 생성

--- src/main/java/com/example/demo/service/UserJdbcApiDao.java
+++ src/main/java/com/example/demo/service/UserJdbcApiDao.java
@@ -1,11 +1,14 @@
 package com.example.demo.service;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.server.ResponseStatusException;
 
+import javax.sql.DataSource;
 import java.sql.*;
 import java.time.ZoneId;
 
@@ -21,14 +24,26 @@ public class UserJdbcApiDao {
     @Value("${spring.datasource.password}")
     private String password;
 
+    @Value("${spring.datasource.driver-class-name}")
+    private String driver;
+
+    private DataSource dataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(url);
+        config.setUsername(username);
+        config.setPassword(password);
+        config.setDriverClassName(driver);
+
+        HikariDataSource hikariDataSource = new HikariDataSource(config);
+        return hikariDataSource;
+    }
+
     public User findById(int userId) throws SQLException {
         Connection connection = null;   // 1
         Statement statement = null;     // 2
         ResultSet resultSet = null;     // 3
         try {
-            connection = DriverManager.getConnection(   // 1
-                    url, username, password
-            );
+            connection = dataSource().getConnection();  // 1
             statement = connection.createStatement();   // 2
             resultSet = statement.executeQuery(         // 3
                     "SELECT * FROM \"user\" WHERE id = " + userId
