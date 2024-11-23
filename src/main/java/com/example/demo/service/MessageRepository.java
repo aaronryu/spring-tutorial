edd1e4b1c1996e583f3abe8dc7c0431a0d216b6c
Aaron Ryu <fbcndah@gmail.com>
2024-11-24 01:52:33 +0900

    실습 6-24: (Hibernate) 단일 SessionFactory 로 매 요청마다 Session 생성하여 Repostitory 구현 (Entity 관리는 안함)

--- src/main/java/com/example/demo/service/MessageRepository.java
+++ src/main/java/com/example/demo/service/MessageRepository.java
@@ -6,6 +6,9 @@ import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.TypedQuery;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.server.ResponseStatusException;
@@ -17,32 +20,34 @@ import java.util.List;
 @RequiredArgsConstructor
 public class MessageRepository {
     private final EntityManagerFactory entityManagerFactory;
+//  SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory(); // @Bean 으로 주입받아도 된다.
 
     public List<Message> findByUserId(int userId) {
-        // 1) EntityManagerFactory : 요청마다 EntityManager 생성
-        // 2) EntityManager : 요청에 따른 EntityManager 생성 완료
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
-        // 3) EntityTransaction : 작업 수행을 위한 트랜잭션 설정 및 쿼리 수행
-        EntityTransaction transaction = entityManager.getTransaction();
-        // 3.1) EntityTransaction 트랜잭션 시작
+        // 1) SessionFactory : 요청마다 Session 생성 <- EntityManagerFactory : 요청마다 EntityManager 생성
+        SessionFactory sessionFactory = entityManagerFactory.unwrap(SessionFactory.class);
+        // 2) Session : 요청에 따른 Session 생성 완료 <- EntityManager : 요청에 따른 EntityManager 생성 완료
+        Session session = sessionFactory.openSession();
+        // 3) Transaction <- EntityTransaction : 작업 수행을 위한 트랜잭션 설정 및 쿼리 수행
+        Transaction transaction = session.getTransaction(); // session.beginTransaction() 은 최초 트랜잭션 선언 시 (이미 SpringProxyHandler 에 의해 생성되어있음)
+        // 3.1) Transaction 트랜잭션 시작 <- EntityTransaction
         transaction.begin();
         try {
             String jpql = "SELECT message From Message message where message.userId = :userId";
-            TypedQuery<Message> typedQuery = entityManager.createQuery(jpql, Message.class);
+            TypedQuery<Message> typedQuery = session.createQuery(jpql, Message.class);
             typedQuery.setParameter("userId", userId);
             List<Message> messages = typedQuery.getResultList();
-            // 3.2) EntityTransaction 트랜잭션 커밋(Commit)
+            // 3.2) Transaction 트랜잭션 커밋(Commit) <- EntityTransaction
             transaction.commit();
             return messages;
         } catch (Exception e) {
-            // 3.3) EntityTransaction 트랜잭션 실패(Rollback)
+            // 3.3) Transaction 트랜잭션 실패(Rollback) <- EntityTransaction
             transaction.rollback();
             log.error(e.getMessage(), e);
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "자원에 대한 접근에 문제가 있습니다.");
         } finally {
-            // 2.1) EntityManager 종료 (Close)
-            entityManager.close();
-            // 1.1) EntityManagerFactory 종료 (Close)
+            // 2.1) Session 종료 (Close) <- EntityManager
+            session.close();
+            // 1.1) SessionFactory 종료 (Close) <- EntityManagerFactory
 //          entityManagerFactory.close();
         }
     }
@@ -51,35 +56,36 @@ public class MessageRepository {
         if (true) {
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "트랜잭션 롤백 여부를 확인하기 위한 의도된 예외");
         }
-        // 1) EntityManagerFactory : 요청마다 EntityManager 생성
-        // 2) EntityManager : 요청에 따른 EntityManager 생성 완료
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
-        // 3) EntityTransaction : 작업 수행을 위한 트랜잭션 설정 및 쿼리 수행
-        EntityTransaction transaction = entityManager.getTransaction();
-        // 3.1) EntityTransaction 트랜잭션 시작
+        // 1) SessionFactory : 요청마다 Session 생성 <- EntityManagerFactory : 요청마다 EntityManager 생성
+        SessionFactory sessionFactory = entityManagerFactory.unwrap(SessionFactory.class);
+        // 2) Session : 요청에 따른 Session 생성 완료 <- EntityManager : 요청에 따른 EntityManager 생성 완료
+        Session session = sessionFactory.openSession();
+        // 3) Transaction <- EntityTransaction : 작업 수행을 위한 트랜잭션 설정 및 쿼리 수행
+        Transaction transaction = session.getTransaction(); // session.beginTransaction() 은 최초 트랜잭션 선언 시 (이미 SpringProxyHandler 에 의해 생성되어있음)
+        // 3.1) Transaction 트랜잭션 시작 <- EntityTransaction
         transaction.begin();
         try {
             // INSERT
-            entityManager.persist(entity); // 영구저장
-            entityManager.flush();
+            session.persist(entity); // 영구저장
+            session.flush();
             Integer createdMessageId = entity.getId();
             // SELECT
             String jpql = "SELECT message From Message message where message.id = :id";
-            TypedQuery<Message> typedQuery = entityManager.createQuery(jpql, Message.class);
+            TypedQuery<Message> typedQuery = session.createQuery(jpql, Message.class);
             typedQuery.setParameter("id", createdMessageId);
             Message message = typedQuery.getSingleResult();
-            // 3.2) EntityTransaction 트랜잭션 커밋(Commit)
+            // 3.2) Transaction 트랜잭션 커밋(Commit) <- EntityTransaction
             transaction.commit();
             return message;
         } catch (Exception e) {
-            // 3.3) EntityTransaction 트랜잭션 실패(Rollback)
+            // 3.3) Transaction 트랜잭션 실패(Rollback) <- EntityTransaction
             transaction.rollback();
             log.error(e.getMessage(), e);
-            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "유저 저장에 실패했습니다.");
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "자원에 대한 접근에 문제가 있습니다.");
         } finally {
-            // 2.1) EntityManager 종료 (Close)
-            entityManager.close();
-            // 1.1) EntityManagerFactory 종료 (Close)
+            // 2.1) Session 종료 (Close) <- EntityManager
+            session.close();
+            // 1.1) SessionFactory 종료 (Close) <- EntityManagerFactory
 //          entityManagerFactory.close();
         }
     }
--- src/main/java/com/example/demo/service/UserRepository.java
+++ src/main/java/com/example/demo/service/UserRepository.java
@@ -3,6 +3,9 @@ package com.example.demo.service;
 import jakarta.persistence.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.server.ResponseStatusException;
@@ -15,121 +18,126 @@ import java.util.*;
 @RequiredArgsConstructor
 public class UserRepository implements IRepository<Integer, User> {
     private final EntityManagerFactory entityManagerFactory;
+//  SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory(); // @Bean 으로 주입받아도 된다.
 
     public Optional<User> findById(Integer id) {
-        // 1) EntityManagerFactory : 요청마다 EntityManager 생성
-        // 2) EntityManager : 요청에 따른 EntityManager 생성 완료
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
-        // 3) EntityTransaction : 작업 수행을 위한 트랜잭션 설정 및 쿼리 수행
-        EntityTransaction transaction = entityManager.getTransaction();
-        // 3.1) EntityTransaction 트랜잭션 시작
+        // 1) SessionFactory : 요청마다 Session 생성 <- EntityManagerFactory : 요청마다 EntityManager 생성
+        SessionFactory sessionFactory = entityManagerFactory.unwrap(SessionFactory.class);
+        // 2) Session : 요청에 따른 Session 생성 완료 <- EntityManager : 요청에 따른 EntityManager 생성 완료
+        Session session = sessionFactory.openSession();
+        // 3) Transaction <- EntityTransaction : 작업 수행을 위한 트랜잭션 설정 및 쿼리 수행
+        Transaction transaction = session.getTransaction(); // session.beginTransaction() 은 최초 트랜잭션 선언 시 (이미 SpringProxyHandler 에 의해 생성되어있음)
+        // 3.1) Transaction 트랜잭션 시작 <- EntityTransaction
         transaction.begin();
         try {
             String jpql = "SELECT user From User user where user.id = :id";
-            TypedQuery<User> typedQuery = entityManager.createQuery(jpql, User.class);
+            TypedQuery<User> typedQuery = session.createQuery(jpql, User.class);
             typedQuery.setParameter("id", id);
             User user = typedQuery.getSingleResult();
-            // 3.2) EntityTransaction 트랜잭션 커밋(Commit)
+            // 3.2) Transaction 트랜잭션 커밋(Commit) <- EntityTransaction
             transaction.commit();
             return Optional.ofNullable(user);
         } catch (Exception e) {
-            // 3.3) EntityTransaction 트랜잭션 실패(Rollback)
+            // 3.3) Transaction 트랜잭션 실패(Rollback) <- EntityTransaction
             transaction.rollback();
             log.error(e.getMessage(), e);
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "자원에 대한 접근에 문제가 있습니다.");
         } finally {
-            // 2.1) EntityManager 종료 (Close)
-            entityManager.close();
-            // 1.1) EntityManagerFactory 종료 (Close)
+            // 2.1) Session 종료 (Close) <- EntityManager
+            session.close();
+            // 1.1) SessionFactory 종료 (Close) <- EntityManagerFactory
 //          entityManagerFactory.close();
         }
     }
 
     public List<User> findAll() {
-        // 1) EntityManagerFactory : 요청마다 EntityManager 생성
-        // 2) EntityManager : 요청에 따른 EntityManager 생성 완료
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
-        // 3) EntityTransaction : 작업 수행을 위한 트랜잭션 설정 및 쿼리 수행
-        EntityTransaction transaction = entityManager.getTransaction();
-        // 3.1) EntityTransaction 트랜잭션 시작
+        // 1) SessionFactory : 요청마다 Session 생성 <- EntityManagerFactory : 요청마다 EntityManager 생성
+        SessionFactory sessionFactory = entityManagerFactory.unwrap(SessionFactory.class);
+        // 2) Session : 요청에 따른 Session 생성 완료 <- EntityManager : 요청에 따른 EntityManager 생성 완료
+        Session session = sessionFactory.openSession();
+        // 3) Transaction <- EntityTransaction : 작업 수행을 위한 트랜잭션 설정 및 쿼리 수행
+        Transaction transaction = session.getTransaction(); // session.beginTransaction() 은 최초 트랜잭션 선언 시 (이미 SpringProxyHandler 에 의해 생성되어있음)
+        // 3.1) Transaction 트랜잭션 시작 <- EntityTransaction
         transaction.begin();
         try {
             String jpql = "SELECT user From User user";
-            List<User> users = entityManager.createQuery(jpql, User.class).getResultList();
+            List<User> users = session.createQuery(jpql, User.class).getResultList();
             transaction.commit();
             return users;
         } catch (Exception e) {
-            // 3.3) EntityTransaction 트랜잭션 실패(Rollback)
+            // 3.3) Transaction 트랜잭션 실패(Rollback) <- EntityTransaction
             transaction.rollback();
             log.error(e.getMessage(), e);
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "자원에 대한 접근에 문제가 있습니다.");
         } finally {
-            // 2.1) EntityManager 종료 (Close)
-            entityManager.close();
-            // 1.1) EntityManagerFactory 종료 (Close)
+            // 2.1) Session 종료 (Close) <- EntityManager
+            session.close();
+            // 1.1) SessionFactory 종료 (Close) <- EntityManagerFactory
 //          entityManagerFactory.close();
         }
     }
 
     public User save(User entity) {
-        // 1) EntityManagerFactory : 요청마다 EntityManager 생성
-        // 2) EntityManager : 요청에 따른 EntityManager 생성 완료
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
-        // 3) EntityTransaction : 작업 수행을 위한 트랜잭션 설정 및 쿼리 수행
-        EntityTransaction transaction = entityManager.getTransaction();
-        // 3.1) EntityTransaction 트랜잭션 시작
+        // 1) SessionFactory : 요청마다 Session 생성 <- EntityManagerFactory : 요청마다 EntityManager 생성
+        SessionFactory sessionFactory = entityManagerFactory.unwrap(SessionFactory.class);
+        // 2) Session : 요청에 따른 Session 생성 완료 <- EntityManager : 요청에 따른 EntityManager 생성 완료
+        Session session = sessionFactory.openSession();
+        // 3) Transaction <- EntityTransaction : 작업 수행을 위한 트랜잭션 설정 및 쿼리 수행
+        Transaction transaction = session.getTransaction(); // session.beginTransaction() 은 최초 트랜잭션 선언 시 (이미 SpringProxyHandler 에 의해 생성되어있음)
+        // 3.1) Transaction 트랜잭션 시작 <- EntityTransaction
         transaction.begin();
         try {
             // INSERT
-            entityManager.persist(entity); // 영구저장
-            entityManager.flush();
+            session.persist(entity); // 영구저장
+            session.flush();
             Integer createdUserId = entity.getId();
             // SELECT
             String jpql = "SELECT user From User user where user.id = :id";
-            TypedQuery<User> typedQuery = entityManager.createQuery(jpql, User.class);
+            TypedQuery<User> typedQuery = session.createQuery(jpql, User.class);
             typedQuery.setParameter("id", createdUserId);
             User user = typedQuery.getSingleResult();
-            // 3.2) EntityTransaction 트랜잭션 커밋(Commit)
+            // 3.2) Transaction 트랜잭션 커밋(Commit) <- EntityTransaction
             transaction.commit();
             return user;
         } catch (Exception e) {
-            // 3.3) EntityTransaction 트랜잭션 실패(Rollback)
+            // 3.3) Transaction 트랜잭션 실패(Rollback) <- EntityTransaction
             transaction.rollback();
             log.error(e.getMessage(), e);
-            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "유저 저장에 실패했습니다.");
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "자원에 대한 접근에 문제가 있습니다.");
         } finally {
-            // 2.1) EntityManager 종료 (Close)
-            entityManager.close();
-            // 1.1) EntityManagerFactory 종료 (Close)
+            // 2.1) Session 종료 (Close) <- EntityManager
+            session.close();
+            // 1.1) SessionFactory 종료 (Close) <- EntityManagerFactory
 //          entityManagerFactory.close();
         }
     }
 
     public void deleteById(Integer id) {
-        // 1) EntityManagerFactory : 요청마다 EntityManager 생성
-        // 2) EntityManager : 요청에 따른 EntityManager 생성 완료
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
-        // 3) EntityTransaction : 작업 수행을 위한 트랜잭션 설정 및 쿼리 수행
-        EntityTransaction transaction = entityManager.getTransaction();
-        // 3.1) EntityTransaction 트랜잭션 시작
+        // 1) SessionFactory : 요청마다 Session 생성 <- EntityManagerFactory : 요청마다 EntityManager 생성
+        SessionFactory sessionFactory = entityManagerFactory.unwrap(SessionFactory.class);
+        // 2) Session : 요청에 따른 Session 생성 완료 <- EntityManager : 요청에 따른 EntityManager 생성 완료
+        Session session = sessionFactory.openSession();
+        // 3) Transaction <- EntityTransaction : 작업 수행을 위한 트랜잭션 설정 및 쿼리 수행
+        Transaction transaction = session.getTransaction(); // session.beginTransaction() 은 최초 트랜잭션 선언 시 (이미 SpringProxyHandler 에 의해 생성되어있음)
+        // 3.1) Transaction 트랜잭션 시작 <- EntityTransaction
         transaction.begin();
         try {
             // DELETE
             String jpql = "DELETE From User where id = :id";
-            TypedQuery<User> typedQuery = entityManager.createQuery(jpql, User.class);
+            TypedQuery<User> typedQuery = session.createQuery(jpql, User.class);
             typedQuery.setParameter("id", id);
             typedQuery.executeUpdate();
-            // 3.2) EntityTransaction 트랜잭션 커밋(Commit)
+            // 3.2) Transaction 트랜잭션 커밋(Commit) <- EntityTransaction
             transaction.commit();
         } catch (Exception e) {
-            // 3.3) EntityTransaction 트랜잭션 실패(Rollback)
+            // 3.3) Transaction 트랜잭션 실패(Rollback) <- EntityTransaction
             transaction.rollback();
             log.error(e.getMessage(), e);
-            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "유저 저장에 실패했습니다.");
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "자원에 대한 접근에 문제가 있습니다.");
         } finally {
-            // 2.1) EntityManager 종료 (Close)
-            entityManager.close();
-            // 1.1) EntityManagerFactory 종료 (Close)
+            // 2.1) Session 종료 (Close) <- EntityManager
+            session.close();
+            // 1.1) SessionFactory 종료 (Close) <- EntityManagerFactory
 //          entityManagerFactory.close();
         }
     }
