252ed2a5a597fc844afb946b5ba6593cc04c4762
Aaron Ryu <fbcndah@gmail.com>
2025-06-13 22:57:51 +0900

    실습 7-12: 기존 대시보드(/) 페이지에 이어 마이페이지(/mypage)와 어드민(/admin) 추가

--- src/main/java/com/example/demo/controller/PageController.java
+++ src/main/java/com/example/demo/controller/PageController.java
@@ -1,19 +1,26 @@
 package com.example.demo.controller;
 
 import com.example.demo.controller.dto.UserResponseDto;
+import com.example.demo.service.UserService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Slf4j
 @Controller
 @RequestMapping("/")
+@RequiredArgsConstructor
 public class PageController {
+    private final UserService userService;
 
     @GetMapping("")
     public String index() {
@@ -27,5 +34,23 @@ public class PageController {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         log.info(" - Anonymous User : " + authentication);
         return "login";
+    }
+
+    @GetMapping("/mypage")
+    public String mypage(Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        log.info(" - Logged User : " + authentication);
+        String principle = (String) authentication.getPrincipal();
+        model.addAttribute("username", principle);
+        return "mypage/index";
+    }
+
+    @GetMapping("/admin")
+    public String admin(Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        log.info(" - Logged User : " + authentication);
+        List<UserResponseDto> users = userService.findAll();
+        model.addAttribute("users", users);
+        return "admin/index";
     }
 }
