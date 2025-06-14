03f2dd3a1f97a0bcab7e7d3a06617c80eb12ee65
Aaron Ryu <fbcndah@gmail.com>
2025-06-14 19:22:50 +0900

    실습 7-20: 클래스(@Controller) 단위 CORS 설정- 특정 Origin + Preflight 캐싱 1시간

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -12,6 +12,12 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:5173", maxAge = 3600)
+/**
+ * - Only 'http://localhost:5173' origins are allowed.
+ * - The HTTP methods allowed are all @RequestMapping annotation in this class.
+ * - The time that the preflight response is cached (maxAge) is 60 minutes(1 hour).
+ */
 @RestController
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
