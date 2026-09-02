0f7603a5122bc7d9daf486d87955346a2da179e0
Aaron Ryu <aaron.ryu.dev@gmail.com>
2026-09-02 09:41:38 +0900

    8. 다수 쿼리가 단일 공용 Connection 내 실행된다하더라도 RDBMS 옵션 SET autocommit = 0(OFF) 꺼주어야 하나의 트랜잭션(임시 저장소 내 대기) 가능
    - 자동 커밋이 켜진(ON) 단일 공용 Connection 사용 시 다수 쿼리 실행해도 각 개별 쿼리마다 데이터베이스 내 최종 반영
    - 자동 커밋이 꺼진(OFF) 단일 공용 Connection 에다 다수 쿼리가 실행되어야한다
      - 동일한 Connection 에다 수행한 수많은 다수 쿼리의 결과들은 RDBMS 측 임시 저장소 내 대기
      - 수동 커밋이기때문에 Connection 대여한(DataSource.getConnection) UserService 가 모든 쿼리 수행 후 가장 마지막에 .commit() 호출해줘야 데이터베이스 내 최종 반영
        - 꼭 마지막에 connection.commit() 해주어야하는것을 잊지마라
        - 만약 수행중에 예외가 발생하면 그것을 받아서 connection.rollback() 해주어야한다
          - 그래야 임시 저장소 내 대기중인 앞서 수행했던 다수 쿼리들의 결과를 싹 다 한번에 날릴 수 있다

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -31,13 +31,20 @@ public class UserService {
     public UserResponseDto create(UserCreateRequestDto request) throws SQLException {
         // (1) Connection 획득한 곳에서
         Connection connection = dataSource.getConnection();
-
-        User createdUser = userJdbcApiRepository.create(connection, request.getName(), request.getAge(), request.getJob(), request.getSpecialty());
-        Message createdMessages = messageJdbcApiRepository.create(connection, createdUser.getId(), createdUser.getName() + "님 회원가입 감사드립니다!");
-        UserResponseDto response = UserResponseDto.from(createdUser, Collections.singletonList(createdMessages));
-
-        // (2) Connection 반환을 해주어야한다 = 쿼리를 수행하는 메서드 내부에서 Connection 반환을 하면 다른 쿼리의 Connection 을 방해하는것
-        connection.close();
-        return response;
+        connection.setAutoCommit(false); // * 중요 : 자동 커밋이 꺼진(OFF) 단 하나의 공용 Connection - 그래야 이 Connection 내부에서 실행되는 모든 쿼리들은 각각 쿼리 실행 시 데이터베이스 내 최종 반영되지 않고 임시 저장소에서 대기할 수 있다
+        try {
+            User createdUser = userJdbcApiRepository.create(connection, request.getName(), request.getAge(), request.getJob(), request.getSpecialty());
+            Message createdMessages = messageJdbcApiRepository.create(connection, createdUser.getId(), createdUser.getName() + "님 회원가입 감사드립니다!");
+            UserResponseDto response = UserResponseDto.from(createdUser, Collections.singletonList(createdMessages));
+            connection.commit(); // * 자동 커밋이 꺼져있기때문에(OFF) 임시 저장소에 쌓여있는 그동안의 쿼리 결과들을 수동 커밋 COMMIT 통해 단 한방에 데이터베이스에 그 모든것들을 최종 반영해야한다
+            return response;
+        } catch (Exception e) {
+            connection.rollback(); // * 자동 커밋이 꺼져있기때문에(OFF) 임시 저장소에 쌓여있는 그동안의 쿼리 결과들을 ROLLBACK 통해 단 한방에 날려버릴 수 있다
+            throw e;
+        } finally {
+            // (2) Connection 반환을 해주어야한다 = 쿼리를 수행하는 메서드 내부에서 Connection 반환을 하면 다른 쿼리의 Connection 을 방해하는것
+            connection.setAutoCommit(true); // * 중요 : DataSource 본질적으로 Connection 재사용이기때문에 우리가 사용할때 어떤 설정을 했다면 그 더러운 상태로 반환하지말고 원상복구(모든 옵션들을 다시 원점복귀)하여 반환해야한다
+            connection.close();
+        }
     }
 }
