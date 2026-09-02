0dba2a52397c80df1f3c203e561c4556ebc9f1be
Aaron Ryu <aaron.ryu.dev@gmail.com>
2026-09-02 09:29:38 +0900

    6. JDBC API 쿼리 실행 메서드 내 '매번 DataSource 로부터 새로운 Connection 가져다 사용 = 단일 Connection 내 다수 쿼리 실행이라는 트랜잭션 전제 자체에 미부합
    - 100개의 쿼리를 수행하면 다 개별적으로 새로운 Connection 가져다가 사용
      - 트랜잭션이라는건 (단 한개의) 단일 공용 Connection 에 다수 쿼리를 보내야하는것
      - 하지만 각 쿼리가 각자 다른 Conneciton 에서 수행된다는건 애당초 트랜잭션 전제에 부합하지 않는것

--- src/main/java/com/example/demo/repository/MessageJdbcApiRepository.java
+++ src/main/java/com/example/demo/repository/MessageJdbcApiRepository.java
@@ -53,6 +53,7 @@ public class MessageJdbcApiRepository {
         try {
             // INSERT 유저 정보
             connection = dataSource.getConnection();
+            if (userId > 2) throw new RuntimeException("같은 하나의 트랜잭션 내 예외 발생 시 ROLLBACK 되는지 확인하기 위해 일부러 유저 ID 3부터는 메세지 저장을 시도할 시 에러를 발생시킵니다");
             statement = connection.prepareStatement("INSERT INTO message (message, user_id, created_at) VALUES (?, ?, ?);");
             statement.setString(1, message);
             statement.setInt(2, userId);
