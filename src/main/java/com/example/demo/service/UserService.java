0350720b30afadb88ed10097f0cfbc9c64fcca24
Aaron Ryu <aaron.ryu.dev@gmail.com>
2026-09-02 10:09:12 +0900

    10. 자동 커밋을 끈(OFF) 단일 Connection 을 ThreadLocal 내 저장하기 위한 트랜잭션 사전준비 / 커밋 후 ThreadLocal 및 Connection 정리하는 트랜잭션 사후정리
    - 복잡한 트랜잭션 사전준비 / 트랜잭션 사후정리 모두 트랜잭션 추상화를 통해 단 메서드 3개(2개 분류의 메서드)로 매우 단순화
      - 트랜잭션 시작: PlatformTransactionManager.getConnection
      - 트랜잭션 종료: PlatformTransactionManager.commit, PlatformTransactionManager.rollback
    - 트랜잭션 사전준비 > 다수 쿼리 실행 및 COMMIT / ROLLBACK 전
      - ThreadLocal 내 단일 Connection 저장 / 삭제로 관리하는 TransactionSynchronizationManager 시작
      - DataSource 로부터 Connection 대여하여 가져오기
      - Connection 의 자동 커밋 설정 끄기 OFF - 하나의 트랜잭션으로 다수 쿼리 결과들을 데이터베이스에 적용하기전 임시 저장소로 유보하기 위함
      - TransactionSynchronizationManager 통해 ThreadLocal 내 단일 Connection 저장
    - 트랜잭션 사후정리 < 다수 쿼리 실행 및 COMMIT / ROLLBACK 후
      - TransactionSynchronizationManager 통해 ThreadLocal 내 단일 Connection 삭제
      - Connection 의 자동 커밋 설정 켜기 ON - 다시 이 Connection 이 DataSource 의 Connection Pool 로 돌아간다면 다른 사람이 사용할것이라 깨끗하게 만들어 반환하자
      - DataSource 에게 Connection 반환하기
      - ThreadLocal 내 단일 Connection 저장 / 삭제로 관리하는 TransactionSynchronizationManager 종료

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -8,7 +8,11 @@ import com.example.demo.repository.User;
 import com.example.demo.repository.UserJdbcApiRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.datasource.ConnectionHolder;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 import org.springframework.transaction.support.TransactionSynchronizationManager;
 
 import javax.sql.DataSource;
@@ -21,6 +25,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class UserService {
     private final DataSource dataSource;
+//  private final PlatformTransactionManager platformTransactionManager;
     private final UserJdbcApiRepository userJdbcApiRepository;
     private final MessageJdbcApiRepository messageJdbcApiRepository;
 
@@ -31,26 +36,17 @@ public class UserService {
     }
 
     public UserResponseDto create(UserCreateRequestDto request) throws SQLException {
-        TransactionSynchronizationManager.initSynchronization();
-        // (1) Connection 획득한 곳에서
-        Connection connection = dataSource.getConnection();
-        connection.setAutoCommit(false); // * 중요 : 자동 커밋이 꺼진(OFF) 단 하나의 공용 Connection - 그래야 이 Connection 내부에서 실행되는 모든 쿼리들은 각각 쿼리 실행 시 데이터베이스 내 최종 반영되지 않고 임시 저장소에서 대기할 수 있다
-        TransactionSynchronizationManager.bindResource(dataSource, new ConnectionHolder(connection)); // 트랜잭션 동기화 : 쓰기
+        PlatformTransactionManager platformTransactionManager = new DataSourceTransactionManager(dataSource);
+        TransactionStatus transactionStatus = platformTransactionManager.getTransaction(new DefaultTransactionDefinition());
         try {
             User createdUser = userJdbcApiRepository.create(request.getName(), request.getAge(), request.getJob(), request.getSpecialty());
             Message createdMessages = messageJdbcApiRepository.create(createdUser.getId(), createdUser.getName() + "님 회원가입 감사드립니다!");
             UserResponseDto response = UserResponseDto.from(createdUser, Collections.singletonList(createdMessages));
-            connection.commit(); // * 자동 커밋이 꺼져있기때문에(OFF) 임시 저장소에 쌓여있는 그동안의 쿼리 결과들을 수동 커밋 COMMIT 통해 단 한방에 데이터베이스에 그 모든것들을 최종 반영해야한다
+            platformTransactionManager.commit(transactionStatus); // * 자동 커밋이 꺼져있기때문에(OFF) 임시 저장소에 쌓여있는 그동안의 쿼리 결과들을 수동 커밋 COMMIT 통해 단 한방에 데이터베이스에 그 모든것들을 최종 반영해야한다
             return response;
         } catch (Exception e) {
-            connection.rollback(); // * 자동 커밋이 꺼져있기때문에(OFF) 임시 저장소에 쌓여있는 그동안의 쿼리 결과들을 ROLLBACK 통해 단 한방에 날려버릴 수 있다
+            platformTransactionManager.rollback(transactionStatus); // * 자동 커밋이 꺼져있기때문에(OFF) 임시 저장소에 쌓여있는 그동안의 쿼리 결과들을 ROLLBACK 통해 단 한방에 날려버릴 수 있다
             throw e;
-        } finally {
-            TransactionSynchronizationManager.unbindResource(dataSource); // 트랜잭션 동기화 : 삭제(D)
-            // (2) Connection 반환을 해주어야한다 = 쿼리를 수행하는 메서드 내부에서 Connection 반환을 하면 다른 쿼리의 Connection 을 방해하는것
-            connection.setAutoCommit(true); // * 중요 : DataSource 본질적으로 Connection 재사용이기때문에 우리가 사용할때 어떤 설정을 했다면 그 더러운 상태로 반환하지말고 원상복구(모든 옵션들을 다시 원점복귀)하여 반환해야한다
-            connection.close();
-            TransactionSynchronizationManager.clearSynchronization();
         }
     }
 }
