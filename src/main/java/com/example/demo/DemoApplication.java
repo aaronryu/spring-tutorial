56da6ea14a437628150e910f2840374a1e4f154c
Aaron Ryu <aaron.ryu.dev@gmail.com>
2026-09-18 18:38:57 +0900

    4-1번째: e.printStackTrace() 쓰지말고 SLF4J(Logback 구현체) 사용하여 로그 출력할것

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,7 +1,10 @@
 package com.example.demo;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.sql.SQLException;
 
+@Slf4j
 public class DemoApplication {
 
     // 1개 계정밖에 허용되지 않는 데이터베이스 - username: "admin" + password: "1234"
@@ -18,7 +21,7 @@ public class DemoApplication {
         try {
             connect("aaron", "7890");
         } catch (SQLException e) {
-            e.printStackTrace();
+            log.error(e.getMessage(), e);
         }
         System.out.println(" - 프로그램이 중간에 멈추지 않고, 정상적으로 종료되었습니다 = exit code 0");
     }
