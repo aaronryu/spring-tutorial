44d0ea4bc9784a616619907eda2d74ed9e15b3e3
Aaron Ryu <aaron.ryu.dev@gmail.com>
2026-09-18 18:28:27 +0900

    3-2번째: Checked Exception (2) 해결책 - throws 통해 나를 호출하려거든 그 예외의 무게를 견뎌라(무책임) > exit code 1 (비정상 종료)

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,19 +5,15 @@ import java.sql.SQLException;
 public class DemoApplication {
 
     // 1개 계정밖에 허용되지 않는 데이터베이스 - username: "admin" + password: "1234"
-    public static void connect(String username, String password) {
-        try {
-            if (username.equals("admin") && password.equals("1234")) {
-                System.out.println("- 데이터베이스 접속 성공");
-            } else {
-                throw new SQLException("데이터베이스 접속 실패 - 사유 : ID / PW 일치하지 않음");
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
+    public static void connect(String username, String password) throws SQLException {
+        if (username.equals("admin") && password.equals("1234")) {
+            System.out.println("- 데이터베이스 접속 성공");
+        } else {
+            throw new SQLException("데이터베이스 접속 실패 - 사유 : ID / PW 일치하지 않음");
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         System.out.println(" - 프로그램이 시작되었습니다");
         connect("aaron", "7890");
         System.out.println(" - 프로그램이 중간에 멈추지 않고, 정상적으로 종료되었습니다 = exit code 0");
