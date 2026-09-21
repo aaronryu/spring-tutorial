9cd8a2194700b5c67ad021eb18742f68af84b650
Aaron Ryu <aaron.ryu.dev@gmail.com>
2026-09-21 16:48:49 +0900

    ② 로깅 - 3번째: 로그 출력 시 매번 원하는 정보를 파라미터로 따라다니며 로그에 넣어주기 귀찮은 경우 MDC 사용 - 단, Encoder-Pattern 내 정의 필요

--- src/main/java/com/example/demo/controller/LoggingController.java
+++ src/main/java/com/example/demo/controller/LoggingController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,10 +19,12 @@ public class LoggingController {
     @RequestMapping(method = RequestMethod.GET, value = "")
     public void logging(HttpServletRequest request) {
         String requestIp = request.getRemoteAddr();
-        log.trace("1: TRACE 로그 - {}", requestIp);
-        log.debug("2: DEBUG 로그 - {}", requestIp);
-        log.info ("3: INFO  로그 - {}", requestIp);
-        log.warn ("4: WARN  로그 - {}", requestIp);
-        log.error("5: ERROR 로그 - {}", requestIp);
+        MDC.put("requested", requestIp);
+        log.trace("1: TRACE 로그"); // log.trace("1: TRACE 로그 - {}", requestIp);
+        log.debug("2: DEBUG 로그"); // log.debug("2: DEBUG 로그 - {}", requestIp);
+        log.info ("3: INFO  로그"); // log.info ("3: INFO  로그 - {}", requestIp);
+        log.warn ("4: WARN  로그"); // log.warn ("4: WARN  로그 - {}", requestIp);
+        log.error("5: ERROR 로그"); // log.error("5: ERROR 로그 - {}", requestIp);
+        MDC.clear();
     }
 }
