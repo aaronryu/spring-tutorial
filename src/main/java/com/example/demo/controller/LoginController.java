1ddb0605555906a497e46930771a06a01ea35e9c
Aaron Ryu <aaron.ryu.dev@gmail.com>
2026-09-21 14:29:50 +0900

    14번째: Bean Validation 입력/요청 파라미터를 사용하는 목적에 따라 검증을 달리하기 위한 검증 그룹 - 단일 LoginRequestDto 를 (1) 로그인 + (2) 회원가입 모두에 사용할때 각 다른 검증이 필요한 경우

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -3,11 +3,14 @@ package com.example.demo.controller;
 import com.example.demo.controller.dto.LoginRequestDto;
 import com.example.demo.controller.dto.LoginResponseDto;
 import com.example.demo.service.LoginService;
+import com.example.demo.validation.OnLogin;
+import com.example.demo.validation.OnRegistration;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
@@ -30,8 +33,16 @@ public class LoginController {
     @ResponseStatus(HttpStatus.ACCEPTED)
     @RequestMapping(method = RequestMethod.POST, value = "/login", consumes = MediaType.APPLICATION_JSON_VALUE)
 //  public LoginResponseDto connect(@RequestBody @Valid LoginRequestDto request) {
-    public LoginResponseDto<Void> connect(@RequestBody LoginRequestDto request) {
+    public LoginResponseDto<Void> connect(@RequestBody @Validated(OnLogin.class) LoginRequestDto request) {
         loginService.connect(request);
+        return LoginResponseDto.success(request.getUsername());
+    }
+
+    @ResponseBody
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    @RequestMapping(method = RequestMethod.POST, value = "/registration")
+    public LoginResponseDto<Void> registration(@RequestBody @Validated(OnRegistration.class) LoginRequestDto request) {
+//      loginService.registration(request); - 실제 회원가입 로직에 의해 회원가입이 성공적으로 되었다고 가정
         return LoginResponseDto.success(request.getUsername());
     }
 }
--- src/main/java/com/example/demo/controller/dto/LoginRequestDto.java
+++ src/main/java/com/example/demo/controller/dto/LoginRequestDto.java
@@ -1,5 +1,7 @@
 package com.example.demo.controller.dto;
 
+import com.example.demo.validation.OnLogin;
+import com.example.demo.validation.OnRegistration;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -7,8 +9,10 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public class LoginRequestDto {
-    @Size(min = 4, message = "유저 아이디는 4글자 이상이어야합니다")
+    @Size(groups = OnRegistration.class, min = 8, message = "회원가입 시 최초 유저 아이디는 8글자 이상이어야합니다")
+    @Size(groups = OnLogin.class, min = 4, message = "유저 아이디는 4글자 이상이어야합니다")
     private final String username;
-    @Size(min = 4, message = "유저 패스워드는 4글자 이상이어야합니다")
+    @Size(groups = OnRegistration.class, min = 8, message = "회원가입 시 최초 유저 패스워드는 8글자 이상이어야합니다")
+    @Size(groups = OnLogin.class, min = 4, message = "유저 패스워드는 4글자 이상이어야합니다")
     private final String password;
 }
