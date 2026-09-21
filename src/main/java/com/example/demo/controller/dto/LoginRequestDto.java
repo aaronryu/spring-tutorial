b54dca02643a6b5a6c27978fe1c60f04101a7ca8
Aaron Ryu <aaron.ryu.dev@gmail.com>
2026-09-21 14:16:12 +0900

    13번째: Bean Validation 입력/요청 파라미터에 대한 검증 실패시에 표기할 커스텀 메세지

--- src/main/java/com/example/demo/controller/dto/LoginRequestDto.java
+++ src/main/java/com/example/demo/controller/dto/LoginRequestDto.java
@@ -7,8 +7,8 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public class LoginRequestDto {
-    @Size(min = 4)
+    @Size(min = 4, message = "유저 아이디는 4글자 이상이어야합니다")
     private final String username;
-    @Size(min = 4)
+    @Size(min = 4, message = "유저 패스워드는 4글자 이상이어야합니다")
     private final String password;
 }
