982a35edc6ce92da6a44a0b4d3f82624acb0312b
Aaron Ryu <aaron.ryu.dev@gmail.com>
2026-09-21 16:41:12 +0900

    ② 로깅 - 2번째: 로그 출력 시 원하는 String 값을 {} 중괄호를 통해 간편하게 함께 넣어 출력 가능

--- src/main/java/com/example/demo/controller/LoggingController.java
+++ src/main/java/com/example/demo/controller/LoggingController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
@@ -15,11 +16,12 @@ public class LoggingController {
     @ResponseStatus(HttpStatus.OK)
     @ResponseBody
     @RequestMapping(method = RequestMethod.GET, value = "")
-    public void logging() {
-        log.trace("1: TRACE 로그");
-        log.debug("2: DEBUG 로그");
-        log.info ("3: INFO  로그");
-        log.warn ("4: WARN  로그");
-        log.error("5: ERROR 로그");
+    public void logging(HttpServletRequest request) {
+        String requestIp = request.getRemoteAddr();
+        log.trace("1: TRACE 로그 - {}", requestIp);
+        log.debug("2: DEBUG 로그 - {}", requestIp);
+        log.info ("3: INFO  로그 - {}", requestIp);
+        log.warn ("4: WARN  로그 - {}", requestIp);
+        log.error("5: ERROR 로그 - {}", requestIp);
     }
 }
