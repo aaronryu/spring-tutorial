ae2b14c69d364fa02e8285ff88472f5809a09700
Aaron Ryu <aaron.ryu.dev@gmail.com>
2026-09-21 16:26:46 +0900

    ② 로깅 - 1번째: 로깅 레벨 - 5가지 TRACE > DEBUG > INFO > WARN > ERROR + 패키지 별로 어느 로깅 레벨까지 볼지 세부 조율 가능

---
+++ src/main/java/com/example/demo/controller/LoggingController.java
@@ -0,0 +1,25 @@
+package com.example.demo.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@Slf4j
+@Controller
+public class LoggingController {
+
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    @RequestMapping(method = RequestMethod.GET, value = "")
+    public void logging() {
+        log.trace("1: TRACE 로그");
+        log.debug("2: DEBUG 로그");
+        log.info ("3: INFO  로그");
+        log.warn ("4: WARN  로그");
+        log.error("5: ERROR 로그");
+    }
+}
